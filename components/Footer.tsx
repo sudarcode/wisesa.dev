02f2b7d63d089cbc87f9a39b9888eaf96ebedb78
Sudar Blogger <terateinka@gmail.com>
2026-06-12 04:52:19 +0700

    Update footer copyright year and name

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="text-center text-sm font-doodle p-3 pb-20 md:pb-3">
       <span className="font-sans">©</span>
-      2022 Anvaqta Tangguh Wisesa. All Rights Reserved.
+      2021 - 2026 Nauval Sudarmanto. All Rights Reserved.
     </div>
   );
 };
